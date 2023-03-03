eb6e7184619fa224cbb5d93c790ddff0ad3706a3
Jakab Arpad <a.jakab@student.tudelft.nl>
2023-03-03 18:30:24 +0100

    Push the class ID's

--- commons/src/main/java/commons/Board.java
+++ commons/src/main/java/commons/Board.java
@@ -2,8 +2,19 @@ package commons;
 
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 
 @Entity
 public class Board {
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
 }
--- commons/src/main/java/commons/Task.java
+++ commons/src/main/java/commons/Task.java
@@ -1,8 +1,19 @@
 package commons;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 
 @Entity
 public class Task {
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
 }
