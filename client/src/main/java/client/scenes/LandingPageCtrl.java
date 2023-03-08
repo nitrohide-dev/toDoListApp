c17cf058e26341aed7281a400e31b54baf61bb9f
Jimmy Zheng <j.b.y.zheng@student.tudelft.nl>
2023-03-08 15:49:37 +0100

    added css for the landing page

--- client/src/main/java/client/scenes/LandingPageCtrl.java
+++ client/src/main/java/client/scenes/LandingPageCtrl.java
@@ -3,6 +3,7 @@ package client.scenes;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class LandingPageCtrl {
@@ -10,12 +11,15 @@ public class LandingPageCtrl {
     private final MainCtrl mainCtrl;
     @FXML
     private TextField server_ip;
+    @FXML
+    private Button connect_button;
 
 
     @Inject
-    public LandingPageCtrl(ServerUtils server, MainCtrl mainCtrl) {
+    public LandingPageCtrl(ServerUtils server, MainCtrl mainCtrl, Button connect_button) {
         this.mainCtrl = mainCtrl;
         this.server = server;
+        this.connect_button = connect_button;
     }
 
     public void connect(){
