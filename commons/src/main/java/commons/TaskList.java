a7af5ead5545a0c267959b756065fc47e9e6a26a
Atanas Semov <A.M.Semov@student.tudelft.nl>
2023-03-08 14:54:38 +0100

    Add checkstyle in dev

--- commons/src/main/java/commons/TaskList.java
+++ commons/src/main/java/commons/TaskList.java
@@ -1,6 +1,9 @@
 package commons;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
